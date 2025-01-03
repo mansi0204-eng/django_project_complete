13c69ad194a96e270f88121d36dd15847caf7013
Mansi Kumawat <mansikumawat42137@gmail.com>
2025-01-03 16:10:23 +0530

    03 jan 2025

--- django-project-complete/SOS/SOS/settings.py
+++ django-project-complete/SOS/SOS/settings.py
@@ -136,6 +136,6 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'mansikumawat42137@gmail.com'
-EMAIL_HOST_PASSWORD = 'dbho zkgl yois gmyr'
+EMAIL_HOST_USER = 'abc@gmail.com'
+EMAIL_HOST_PASSWORD = 'pass@123'
 
